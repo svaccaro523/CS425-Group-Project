c8bc411a46a8bdfec98afff7d976bd4882e38856
Jessie Nelson <jnelso23@hawk.iit.edu>
2016-11-21 23:21:24 -0600

    Integrating constraints and indexes

--- deliverable_2.sql
+++ deliverable_2.sql
@@ -1,9 +1,8 @@
-/* drop table consistsOf; */
 drop table pricing;
 drop table orders;
+drop table supplier;
 drop table product;
 drop table customer;
-drop table supplier;
 drop table staffMember;
 drop table warehouse;
 drop table address;
@@ -21,58 +20,30 @@ CREATE TABLE product(
   PRIMARY KEY (productID)
 );
 
-
 CREATE TABLE pricing(
   productID   INT,
-  state       VARCHAR(2),
-  price       INT, 
-  PRIMARY KEY (productID, state),
-  FOREIGN KEY (productID) REFERENCES product
+  st          VARCHAR(2) NOT NULL,
+  price       NUMERIC(5,2), 
+  PRIMARY KEY (productID, st),
+  FOREIGN KEY (productID) REFERENCES product,
+  CHECK (price>=0)
 );
 
-
 CREATE TABLE customer(
-  customerID     INT NOT NULL,
+  customerID     INT,
   currentBalance NUMERIC(6,2),
   customerName   VARCHAR(32),
   age            INT,
   ccNumber       NUMERIC(16,0),
-  PRIMARY KEY(customerID)
+  PRIMARY KEY(customerID),
+  CHECK (age>=0)
 );
-
-CREATE TABLE orders(
-  customerID    INT NOT NULL,
-  productID     INT NOT NULL,
-  orderID       INT NOT NULL, 
-  totalCost     NUMERIC(6,2),
-  status        VARCHAR(8),
-  month         INT,
-  day           INT,
-  year          INT,
-  PRIMARY KEY (customerID, productID, orderID),
-  FOREIGN KEY (customerID) REFERENCES customer,
-  FOREIGN KEY (productID) REFERENCES product	
-);
-
-/*
-CREATE TABLE consistsOf(
-  customerID   INT NOT NULL,
-  productID    INT NOT NULL,
-  orderID      INT NOT NULL, 
-  quantity     INT,
-  PRIMARY KEY (customerID, productID, orderID),
-  FOREIGN KEY (orderID) REFERENCES orders,
-  FOREIGN KEY (customerID) REFERENCES customer,
-  FOREIGN KEY (productID) REFERENCES product
-);
-
-*/
 
 CREATE TABLE address
 (
   addressID      INT,
   city           VARCHAR(16),
-  state          CHAR(2),
+  st             CHAR(2) NOT NULL,
   zip            NUMERIC(5,0),
   streetName     VARCHAR(32),
   streetNumber   NUMERIC(3,0),
@@ -80,14 +51,38 @@ CREATE TABLE address
   PRIMARY KEY (addressID)
 );
 
+CREATE TABLE orders(
+  customerID    INT,
+  productID     INT,
+  orderID       INT, 
+  addressID     INT,
+  quantity      INT,
+  totalCost     NUMERIC(6,2),
+  status        VARCHAR(8),  
+  month         INT,
+  day           INT,
+  year          INT,
+  PRIMARY KEY (customerID, productID, orderID),
+  FOREIGN KEY (customerID) REFERENCES customer,
+  FOREIGN KEY (productID) REFERENCES product,	
+  FOREIGN KEY (addressID) REFERENCES address,
+  CHECK (month BETWEEN 1 AND 12),
+  CHECK (day BETWEEN 1 AND 31),
+  CHECK (year BETWEEN 1950 AND 2050)
+);
+
+
 CREATE TABLE supplier
 (
   name         VARCHAR (32),
   price        NUMERIC (5,2),
   addressID    INT,
   warehouseID  INT,
+  productID    INT,
   PRIMARY KEY (name),
-  FOREIGN KEY (addressID) REFERENCES address
+  FOREIGN KEY (addressID) REFERENCES address,
+  FOREIGN KEY (productID) REFERENCES product,
+  CHECK (price>=0)
 );
 
 CREATE TABLE warehouse(
@@ -102,12 +97,22 @@ CREATE TABLE staffMember
 (
   employeeID    NUMERIC (5,2),
   addressID     INT,
-  firstName     VARCHAR (32),
-  lastName      VARCHAR (32),
-  jobTitle      VARCHAR (16),
+  firstName     VARCHAR (32) NOT NULL,
+  lastName      VARCHAR (32) NOT NULL,
+  jobTitle      VARCHAR (16) NOT NULL,
   salary        NUMERIC (6,2),
   warehouseID   INT,
   PRIMARY KEY (employeeID),
   FOREIGN KEY(addressID) REFERENCES address,
-  FOREIGN KEY(warehouseID) REFERENCES warehouse
+  FOREIGN KEY(warehouseID) REFERENCES warehouse,
+  CHECK (salary>=0)
 );
+
+CREATE INDEX usa
+ON pricing(st);
+
+CREATE INDEX orderFulfillment 
+ON orders(status);
+
+CREATE INDEX ac
+ON product(name, alcoholContent);
